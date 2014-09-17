b92dfac093d34d82e03991c7686d16f9e15f6114
Kevin Kim <kevin@kinda.us>
2014-09-18 08:43:21 +0900

    [FEATURE] Media viewer for evar video and use cases

--- js/evar.js
+++ js/evar.js
@@ -29,3 +29,75 @@ $(document).ready(function() {
     }
   });
 });
+
+$(document).ready(function() {
+  $('.hero-device').on('click', 'a', function(event) {
+    event.preventDefault();
+    var video = document.getElementById('evar-video-element');
+    video.currentTime = 0;
+    video.play();
+
+    if (!$('html').hasClass('shaded')) {
+      $('html').addClass('shaded');
+    }
+
+    $('.media-viewer.visible').removeClass('visible');
+    $('#evar-video-viewer').addClass('visible');
+  });
+});
+
+$(document).ready(function() {
+  $('#view-use-cases').click(function(event) {
+    event.preventDefault();
+    if (!$('html').hasClass('shaded')) {
+      $('html').addClass('shaded');
+    }
+
+    $('.media-viewer.visible').removeClass('visible');
+    $('#evar-use-cases-viewer').addClass('visible');
+  });
+});
+
+$(document).ready(function() {
+  $('.media-viewer').on('click', '.close', function(event) {
+    $('html').removeClass('shaded');
+    $('.media-viewer.visible').removeClass('visible');
+
+    if ($(this).closest('.media-viewer').attr('id') === 'evar-video-viewer') {
+      var video = document.getElementById('evar-video-element');
+      video.pause();
+    }
+  });
+});
+
+$(document).ready(function() {
+  $('#media-viewer-controller-left').on('click', function(event) {
+    event.preventDefault();
+
+    var currentFigure = $('#evar-use-cases-viewer').find('figure:visible');
+    var nextFigure = currentFigure.prev('figure');
+
+    currentFigure.hide();
+
+    if (nextFigure.length) {
+      nextFigure.show();
+    } else {
+      $('#evar-use-cases-viewer').find('figure').last().show();
+    }
+  });
+
+  $('#media-viewer-controller-right').on('click', function(event) {
+    event.preventDefault();
+
+    var currentFigure = $('#evar-use-cases-viewer').find('figure:visible');
+    var nextFigure = currentFigure.next('figure');
+
+    currentFigure.hide();
+
+    if (nextFigure.length) {
+      nextFigure.show();
+    } else {
+      $('#evar-use-cases-viewer').find('figure').first().show();
+    }
+  });
+});
